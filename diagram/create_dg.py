be2edb6dd6224c46d4873bc2d15fb0dc54ebfe46
Grimjo666 <uinstonmartis@gmail.com>
2023-02-25 17:04:49 +0300

    add Create Diagram script

---
+++ diagram/create_dg.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+
+def create_diagram(person_info_list):
+    # Создаем список долей каждого сектора
+    sizes = []
+    # Создаем список меток для каждого сектора
+    labels = []
+
+    for person in person_info_list[:5]:
+
+        labels.append(person[0])
+        sizes.append(person[1])
+
+    colors = [(0.3, 0.4, 0.8), (0.8, 0.3, 0.3), (0.8, 0.6, 0.3), (0.3, 0.8, 0.8)]
+
+    plt.rcParams['figure.facecolor'] = (0.6, 0.8, 0.7)
+
+    # Строим круговую диаграмму на основе списка долей
+    plt.pie(sizes, labels=labels, colors=colors)
+
+    # Добавляем заголовок
+    plt.title('Актив на 119')
+
+    # Сохраняем диаграмму
+    plt.savefig('diagram\statistic_person_activ.png')
